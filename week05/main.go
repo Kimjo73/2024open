c45154351ecfd23bf6f4dd81ae115e4a675d81f9
rlawngus <jhk0503030@gmail.com>
2024-10-11 10:29:49 +0900

    1

---
+++ week05/main.go
@@ -0,0 +1,16 @@
+package main
+
+import ("fmt"
+       "math"
+)
+
+func main() {
+       
+	i := 55
+    fmt.println(math.Cell(3.99))
+
+	fmt.printin("value i : %d\n", i)
+	fmt.printin("value i : ", i, "\n")
+	fmt.printin("value i :", i)
+
+}
